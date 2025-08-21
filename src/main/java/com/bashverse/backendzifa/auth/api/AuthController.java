3bcb7a93a251cedc842d0507797f8736d6a002cc
sadxdev <sadikxdev@gmail.com>
2025-08-21 14:02:34 +0530

    completed auth/refresh-token

--- src/main/java/com/bashverse/backendzifa/auth/api/AuthController.java
+++ src/main/java/com/bashverse/backendzifa/auth/api/AuthController.java
@@ -1,10 +1,13 @@
 package com.bashverse.backendzifa.auth.api;
 
 import com.bashverse.backendzifa.auth.domain.LogoutRequest;
+import com.bashverse.backendzifa.auth.domain.RefreshTokenRequest;
 import com.bashverse.backendzifa.auth.infra.keycloak.KeycloakAdminClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -17,5 +20,11 @@ public class AuthController {
     public ResponseEntity<Void> logout(@RequestBody LogoutRequest request) {
         keycloakAdminClient.logout(request.getRefreshToken());
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<Map<String, Object>> refreshToken(@RequestBody RefreshTokenRequest request) {
+        Map<String, Object> tokens = keycloakAdminClient.refreshToken(request.getRefreshToken());
+        return ResponseEntity.ok(tokens);
     }
 }
